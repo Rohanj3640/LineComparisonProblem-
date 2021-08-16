c258a85d17b9f9cb3487a3721936c5664290cf38
Rohan Jadhav <rohanj121999@gmail.com>
2021-08-15 23:53:26 -0700

    Added: Equality of two lines

--- src/com/Rohanj/LineComparison.java
+++ src/com/Rohanj/LineComparison.java
@@ -1,12 +1,12 @@
 package com.Rohanj;
+
 import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to The Line Comparison Computation Program");
-        // scanner class to take the input from user
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);              // scanner class to take the input from user
         System.out.print("Enter x1 of First Point: ");
         int x1 = sc.nextInt();
         System.out.print("Enter y1 of First Point: ");
@@ -15,12 +15,28 @@ public class LineComparison {
         int x2 = sc.nextInt();
         System.out.print("Enter y2 of Second Point: ");
         int y2 = sc.nextInt();
+
+        System.out.print("Enter x1 of Third Point: ");
+        int p1 = sc.nextInt();
+        System.out.print("Enter y1 of Third Point: ");
+        int q1 = sc.nextInt();
+        System.out.print("Enter x2 of Forth Point: ");
+        int p2 = sc.nextInt();
+        System.out.print("Enter y2 of Forth Point: ");
+        int q2 = sc.nextInt();
         sc.close();
 
-        // calculate the length of line
-        double lengthOfLine = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,2));
+        Double lenghtOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));      // calculate the length of first line
+        Double lengthOfLine2 = Math.sqrt(Math.pow(p2 - p1, 2) + Math.pow(q2 - q1, 2));     // calculate the length of second line
 
-        // %.2f %n to print only two digit after point
-        System.out.printf("Length of Line is : %.2f %n", lengthOfLine);
+        System.out.printf("Length of First Line is : %.2f %n", lenghtOfLine1);               // %.2f %n to print only two digit after point
+        System.out.printf("length of Second Line : %.2f %n", lengthOfLine2);           // %.2f %n to print only two digit after point
+
+        boolean ans = lenghtOfLine1.equals(lengthOfLine2);                             // return true if equal unless false
+        if (ans)
+            System.out.println("Both Lines are Equal");
+        else
+            System.out.println("Both Lines are not Equal");
+
     }
 }
